df65195f17563828ea4b83b982d646f923a7b82c
Angel Hernandez <angelhernandez@Angels-MacBook-Pro.local>
2017-04-10 19:58:48 -0700

    First commit

---
+++ timer.js
@@ -0,0 +1,73 @@
+function updateTimer(deadline){
+  var time = deadline - new Date();
+  return {
+    'days': Math.floor( time/(1000*60*60*24) ),
+    'hours': Math.floor( (time/(1000*60*60)) % 24 ),
+    'minutes': Math.floor( (time/1000/60) % 60 ),
+    'seconds': Math.floor( (time/1000) % 60 ),
+    'total' : time
+  };
+}
+
+
+function animateClock(span){
+  span.className = "turn";
+  setTimeout(function(){
+    span.className = "";
+  },700);
+}
+
+function startTimer(id, deadline){
+  var timerInterval = setInterval(function(){
+    var clock = document.getElementById(id);
+   var timer = updateTimer(deadline);
+
+    clock.innerHTML = '<span>' + timer.days + '</span>'
+                    + '<span>' + timer.hours + '</span>'
+                    + '<span>' + timer.minutes + '</span>'
+                    + '<span>' + timer.seconds + '</span>';
+
+    //animations
+    var spans = clock.getElementsByTagName("span");
+   animateClock(spans[3]);
+    if(timer.seconds == 59) animateClock(spans[2]);
+    if(timer.minutes == 59 && timer.seconds == 59) animateClock(spans[1]);
+    if(timer.hours == 23 && timer.minutes == 59 && timer.seconds == 59) animateClock(spans[0]);
+
+    //check for end of timer
+    if(timer.total < 1){
+      clearInterval(timerInterval);
+      clock.innerHTML = '<span>0</span><span>0</span><span>0</span><span>0</span>';
+    }
+
+
+  }, 1000);
+}
+
+
+window.onload = function(){
+  var deadline = new Date("April 22, 2017 17:15:00");
+  startTimer("clock", deadline);
+};
+
+
+var weather = new XMLHttpRequest();
+weather.open("GET", "http://api.wunderground.com/api/e069d13c9432ad2f/conditions/q/CA/San_Francisco.json", false);
+weather.send(null);
+
+var r = JSON.parse(weather.response);
+
+var dis = "Current location: " + r.current_observation.display_location.full + "  <p>";
+var temp = r.current_observation.temperature_string+ "  <p>";
+var wind = r.current_observation.wind_string+ "  <p>";
+
+function getWeather(id,res) {
+  document.getElementById(id).innerHTML=res;
+}
+getWeather("weather",dis);
+getWeather("temp",temp);
+getWeather("wind",wind);
+
+
+
+
